abac0be62c292a13f475f9d890806e697aa744c2
Deepthi-M555 <mdeepthi555@gmail.com>
2026-06-15 17:32:10 +0530

    feat(dashboard): implement dashboard functionality

---
+++ routes/dashboardRoutes.js
@@ -0,0 +1,47 @@
+const express = require("express");
+
+const router = express.Router();
+
+const protect = require("../middleware/authMiddleware");
+
+const {
+    createTrack,
+    getTracks,
+    getTopics,
+    getTopicTimeline,
+} = require("../controllers/dashboardController");
+
+/*
+Dashboard Cards
+*/
+router.get(
+    "/tracks",
+    protect,
+    getTracks
+);
+
+router.post(
+    "/tracks",
+    protect,
+    createTrack
+);
+
+/*
+Open Track
+*/
+router.get(
+    "/tracks/:trackId/topics",
+    protect,
+    getTopics
+);
+
+/*
+Open Topic Timeline
+*/
+router.get(
+    "/topics/:topicId/activities",
+    protect,
+    getTopicTimeline
+);
+
+module.exports = router;
